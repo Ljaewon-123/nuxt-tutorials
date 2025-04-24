4a71b4ba98770e39680d4cacc32199f6c1f2e00d
Ljaewon-123 <jml6388@naver.com>
2025-04-24 14:21:33 +0900

    nav: circut-breaker test

--- better-auth/app/layouts/default.vue
+++ better-auth/app/layouts/default.vue
@@ -4,7 +4,7 @@
       <ul class="flex gap-4">
         <li><ULink to="/">Home</ULink></li>
         <li><ULink to="/sign-up-in">Sign Up / Sign In</ULink></li>
-        <li><ULink to="/twofactor">2FA</ULink></li>
+        <li><ULink to="/circuit-breaker">Circuit Breaker</ULink></li>
       </ul>
     </nav>
     <main>
