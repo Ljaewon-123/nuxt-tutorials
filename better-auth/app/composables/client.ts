ac1d115ef37d71cad033d8c6cd63fb6cdb7fe4d1
Ljaewon-123 <jml6388@naver.com>
2025-04-28 09:09:30 +0900

    comment

--- better-auth/app/composables/client.ts
+++ better-auth/app/composables/client.ts
@@ -38,8 +38,8 @@ export function useAuth() {
   // 부작용 매우 조심해야하고 아니면 loggedIn같은 상태 넘기는걸 포기해야함
   // %%%%%%%% 이미 라우터 인증을 위한 미들웨어를 사용하는 페이지면 이미 2번 페칭하기때문에 다른방법 고려 
   // 로그 아웃까지는 여기에 포함하는거 생각 해보자
-  // console.log('useAuth', session.value)
-  if(session.value === null) {
+  // 현재 세션이 남아있는지 정확하게 확인할 무언가
+  if(session.value === null) { // 흠...
     fetchSession()
   }
 
--- better-auth/app/middleware/role.ts
+++ better-auth/app/middleware/role.ts
@@ -1,3 +1,4 @@
+// auth.ts 참조 
 export default defineNuxtRouteMiddleware(async (event) => {
   const { session, user } = useAuth()
 
