e40e70ab0b0764d8b88f10461db718c9b67ba7ab
Ljaewon-123 <jml6388@naver.com>
2025-05-27 10:43:10 +0900

    comment: 불필요 기능?

--- better-auth/server/utils/null-safety-handler.ts
+++ better-auth/server/utils/null-safety-handler.ts
@@ -17,7 +17,7 @@ const validateNullSafety = <T>(value: T): Response<NonNullable<T>> => {
 
 
 type VariableType = 'array' | 'object' | 'string' | 'number' | 'boolean' | 'undefined' | 'null' | string
-interface Response<T> {
+interface Response<T> { // array나 object가 아니면 딱히 쓸데가 없을거같은데 
   isEmpty: boolean    // data가 배열일경우 data.langth == 0 , {} 빈객체일경우 true 아니면 false
   valueType: VariableType,  // typeof data  ex) array, object, string, number or customTypes, interfaces that's using only debug
   result: T
@@ -46,5 +46,8 @@ function wrapResponse<T>(value: NonNullable<T>): Response<NonNullable<T>> {
 }
 
 // null safety 하는건 좋은데 array나 object일때는 별로인거 같은데 
+// 배열이나 object에만 Response를 적용하면 일관성은 없는데 쓰기는 편하겠다. 
+
+
 // defineEventHandler에 getParams, getQuery, getBody 이거 유효성 검사 하는거 있을텐데
 // https://h3.dev/examples/validate-data#validate-params
