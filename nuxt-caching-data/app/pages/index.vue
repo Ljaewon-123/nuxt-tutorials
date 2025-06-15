2957aa456d9823f7bad7eb081265c19d909eb644
Ljaewon-123 <jml6388@naver.com>
2025-06-15 16:39:54 +0900

    Update index.vue

--- nuxt-caching-data/app/pages/index.vue
+++ nuxt-caching-data/app/pages/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
-    page 1\
+    page 1 -
     <NuxtTime :datetime="Date.now()" />
+    <div>
+      <p>page messages</p>
+      <div>
+        {{ message }} /
+        {{ politeness }}
+      </div>
+    </div>
     <li>
       <!-- 먼저 렌더링된 놈이 캐시key 우선권을 가져가는거 같음 -->
       <ul>
@@ -15,5 +22,7 @@
 </template>
 
 <script setup lang="ts">
-
+const { message, politeness, set, polite, assertive } = useRouteAnnouncer({
+  politeness: 'assertive'
+})
 </script>
